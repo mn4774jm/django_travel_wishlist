97188d971c9f6e2008542fb6e05fe746f266d05a
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-11-02 16:07:45 -0600

    working to get js to function for "are you sure" message. No warning prompt appears when deleting places.

--- travel_wishlist/models.py
+++ travel_wishlist/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
+# when new models are created the user will need to run python3 manage.py makemigrations
+# followed by python3 manage.py migrate
 class Place(models.Model):
+    # if user is deleted remove all associated places
+    user = models.ForeignKey('auth.user', null=False, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     visited = models.BooleanField(default=False)
+    notes = models.TextField(blank=True, null=True)
+    date_visited = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='user_images/', blank=True, null=True)
 
     def __str__(self):
-        return f'{self.name}, visited? {self.visited}'
+        photo_str = self.photo.url if self.photo else 'no photo'
+        notes_str = self.notes[100:] if self.notes else 'no notes'
+        return f'{self.pk}:  {self.name}, visited? {self.visited} on {self.date_visited}. Notes: {notes_str}.\nPhoto {photo_str}'
--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
+
+@login_required
 def place_list(request):
 
     '''If this is a POST request, the user clicked the Add button
@@ -15,18 +19,20 @@ def place_list(request):
 
     if request.method == 'POST':
         form = NewPlaceForm(request.POST)
-        place = form.save()     # Creates a new Place from the form
+        place = form.save(commit=False)     # Creates a new Place from the form
+        place.user = request.user
         if form.is_valid():     # Checks against DB constraints, for example, are required fields present?
             place.save()        # Saves to the database
             return redirect('place_list')       # redirects to GET view with name place_list - which is the same view
 
     ''' If not a POST, or the form is not valid, render the page
     with the form to add a new place, and list the places'''
-    places = Place.objects.filter(visited=False).order_by('name')
+    places = Place.objects.filter(user=request.user).filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
 
+@login_required
 def places_visited(request):
     visited = Place.objects.filter(visited=True)
     return render(request, 'travel_wishlist/visited.html', {'visited': visited})
@@ -38,10 +44,32 @@ def about(request):
     return render(request, 'travel_wishlist/about.html', {'author': author, 'about': about})
 
 
-def place_was_visited(request, place_pk):
+@login_required
+def place_was_visited(request):
     if request.method == 'POST':
-        place = get_object_or_404(Place, pk=place_pk)
-        place.visited = True
-        place.save()
+        pk = request.POST.get('pk')
+        place = get_object_or_404(Place, pk=pk)
+        print(place.user, request.user)
+        if place.user == request.user:
+            place.visited = True
+            place.save()
+        else:
+            return HttpResponseForbidden()
 
     return redirect('place_list')
+
+
+@login_required
+def place_details(request, place_pk):
+    place = get_object_or_404(Place, pk=place_pk)
+    return render(request, 'travel_wishlist/place_detail.html', {'place': place} )
+
+
+@login_required
+def delete_place(request, place_pk):
+    place = get_object_or_404(Place, pk=place_pk)
+    if place.user == request.user:
+        place.delete()
+        return redirect('place_list')
+    else:
+        return HttpResponseForbidden()
